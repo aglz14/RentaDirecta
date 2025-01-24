c513c0c36ad2384777f785c9d491101682f2686d
Alejandro GR <137854185+aglz14@users.noreply.github.com>
2025-01-24 11:56:04 -0600

    Suscripción Mensual

--- src/components/sections/Pricing.tsx
+++ src/components/sections/Pricing.tsx
@@ -9,9 +9,9 @@ export function Pricing() {
         <div className="text-center mb-16">
           <h2 className="text-lg font-bold text-[#4CAF50] mb-4">PROPIETARIOS</h2>
           <h2 className="text-4xl font-bold text-[#323232] mb-4">Planes Disponibles para tus Inquilinos</h2>
-          <p className="text-xl text-gray-600">Los inquilinos eligen el plan que mejor se adapte a su vida</p>
+          <p className="text-xl text-gray-600">Los inquilinos eligen el plan que mejor se adapte a cada una de tus propiedades</p>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-4xl mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-4xl mx-auto">
           <div className="bg-white p-8 rounded-xl custom-shadow payment-option">
             <h3 className="text-2xl font-bold text-[#323232] mb-4">Suscripción por Quincena</h3>
             <p className="text-gray-600 mb-6">Pago automático quincenal con 5% de descuento</p>
@@ -19,6 +19,31 @@ export function Pricing() {
               <li className="flex items-center text-gray-600">
                 <CheckCircle2 className="h-5 w-5 text-[#00A86B] mr-2" />
                 Descuento del 5% en la renta
+              </li>
+              <li className="flex items-center text-gray-600">
+                <CheckCircle2 className="h-5 w-5 text-[#00A86B] mr-2" />
+                Pagos automáticos
+              </li>
+              <li className="flex items-center text-gray-600">
+                <CheckCircle2 className="h-5 w-5 text-[#00A86B] mr-2" />
+                Sólo se requiere tarjeta de crédito
+              </li>
+              <li className="flex items-center text-gray-600">
+                <CheckCircle2 className="h-5 w-5 text-[#00A86B] mr-2" />
+                Sin cargos adicionales
+              </li>
+            </ul>
+            <Button className="w-full bg-[#00A86B] hover:bg-[#009060] text-white">
+              Seleccionar Plan
+            </Button>
+          </div>
+          <div className="bg-white p-8 rounded-xl custom-shadow payment-option">
+            <h3 className="text-2xl font-bold text-[#323232] mb-4">Suscripción Mensual</h3>
+            <p className="text-gray-600 mb-6">Pago automático mensual con 3% de descuento</p>
+            <ul className="space-y-4 mb-8">
+              <li className="flex items-center text-gray-600">
+                <CheckCircle2 className="h-5 w-5 text-[#00A86B] mr-2" />
+                Descuento del 3% en la renta
               </li>
               <li className="flex items-center text-gray-600">
                 <CheckCircle2 className="h-5 w-5 text-[#00A86B] mr-2" />
